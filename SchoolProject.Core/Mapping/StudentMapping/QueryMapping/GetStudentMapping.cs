0fd92c2448959a8882141f5fab98a7d247bd120f
Hesham Elsayed <heesham749@gmail.com>
2025-05-20 15:49:29 +0300

    Enhance localization in StudentMapper.ToDto method
    Updated the `ToDto` method to improve localization for student names and department names. Replaced `NameEn` with a `Localize` method to support multiple languages, and applied localization to the department name as well. This change enhances the internationalization of the data returned in the `GetStudentDto` object.

--- SchoolProject.Core/Mapping/StudentMapping/QueryMapping/GetStudentMapping.cs
+++ SchoolProject.Core/Mapping/StudentMapping/QueryMapping/GetStudentMapping.cs
@@ -6,8 +6,12 @@ public static partial class StudentMapper
 {
     public static GetStudentDto ToDto(this Student student)
     {
-        return new GetStudentDto(student.StudID, student.NameEn, student.Address,
-            student.Phone, student.Department!.DNameEn,
+        return new GetStudentDto(
+            student.StudID,
+            student.Localize(student.NameAr, student.NameEn),
+            student.Address,
+            student.Phone,
+            student.Department!.Localize(student.Department.DNameAr, student.Department.DNameEn),
             [.. student.Subjects.AsValueEnumerable().Select(subj => subj.SubjectName)]);
     }
 }
