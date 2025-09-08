ae49fdf3f0c451b597c503713c181b4694d18a6f
Hesham749 <heesham749@gmail.com>
2025-09-08 17:37:28 +0300

    u

--- SchoolProject.Domain/Common/LocalizeEntity.cs
+++ SchoolProject.Domain/Common/LocalizeEntity.cs
@@ -2,13 +2,13 @@
 
 public interface ILocalizeEntity
 {
-    string? Localize(string? textAr, string? textEn)
-    {
-        var culture = Thread.CurrentThread.CurrentCulture;
+	string? Localize(string? textAr, string? textEn)
+	{
+		var culture = Thread.CurrentThread.CurrentCulture;
 
-        if (culture.TwoLetterISOLanguageName.Equals("ar", StringComparison.OrdinalIgnoreCase))
-            return textAr;
+		if (culture.TwoLetterISOLanguageName.Equals("ar", StringComparison.OrdinalIgnoreCase))
+			return textAr;
 
-        return textEn;
-    }
+		return textEn;
+	}
 }
