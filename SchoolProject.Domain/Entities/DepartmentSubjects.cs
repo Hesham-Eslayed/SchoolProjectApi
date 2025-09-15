a63b1d9e07092bdc370d9dab398303c2d8d95937
Hesham749 <heesham749@gmail.com>
2025-09-15 16:08:47 +0300

    add identity

---
+++ SchoolProject.Domain/Entities/DepartmentSubjects.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolProject.Domain.Entities;
+
+public class DepartmentsSubjects
+{
+    public int DID { get; set; }
+
+    public int SubID { get; set; }
+
+    public virtual Department Department { get; set; } = null!;
+
+    public virtual Subject Subject { get; set; } = null!;
+}
---
+++ SchoolProject.Domain/Entities/StudentSubjects.cs
@@ -0,0 +1,16 @@
+﻿namespace SchoolProject.Domain.Entities;
+
+public class StudentsSubjects
+{
+
+    public int StudID { get; set; }
+
+    public int SubID { get; set; }
+
+    public decimal? Grade { get; set; }
+
+    public virtual Student Student { get; set; } = null!;
+
+    public virtual Subject Subject { get; set; } = null!;
+
+}
